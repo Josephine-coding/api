dc53e9a5fcedd0994fec1cee51470520957d7bdc
Josephine-coding <josephine.delobel@gmail.com>
2021-07-20 16:20:22 +0200

    separate files

--- app.py
+++ app.py
@@ -1,128 +1,26 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 import pickle
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from jose import JWTError, jwt
-from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
 
-from datetime import datetime, timedelta
-from typing import Optional
+from database import fake_users_db
+from schemas import Token, User
+from utils import authenticate_user, create_access_token, get_current_active_user
+from utils import ACCESS_TOKEN_EXPIRE_MINUTES
 '''
 heroku url : https://lit-spire-48980.herokuapp.com/
 '''
-SECRET_KEY = "b107cd73a42df17cf68b8aae60b7a530c8fddc9ec16a86ae9854d76be25786f1"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
 
 app = FastAPI()
 
 
 @app.get("/{input}")
-def predict(input: str):
-    tfidf, model = pickle.load(open('model.bin', 'rb'))
-    predictions = model.predict(tfidf.transform([input]))
-    label = predictions[0]
-    return {'text': input, 'label': label}
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-def fake_hash_password(password: str):
-    return "fakehashed" + password
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: Optional[str] = None
-
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
-
-class UserInDB(User):
-    hashed_password: str
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
+def predict(input: str, current_user: User = Depends(get_current_active_user)):
+    if current_user:
+        tfidf, model = pickle.load(open('model.bin', 'rb'))
+        predictions = model.predict(tfidf.transform([input]))
+        label = predictions[0]
+        return {'text': input, 'label': label}
 
 
 @app.post("/token", response_model=Token)
